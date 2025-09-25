34e7da33ebc406d25a238b64ad5229e8b5fba465
Xygi666 <91006842+Xygi666@users.noreply.github.com>
2025-09-25 18:17:33 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'uchet2-cache-v3.1.1';
+const CACHE_NAME = 'uchet2-cache-v3.1.2';
 const STATIC_CACHE_URLS = [
   './','./index.html','./styles.css','./app.js','./backup.js','./work-schedule.js','./manifest.json','./icon-192.png','./icon-512.png'
 ];
